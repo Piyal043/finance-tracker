3f75717f319973ec8e0a1fae8498f2041c1c617a
Peyal <mohammad.piyal@gmail.com>
2020-09-29 15:15:17 +0600

    Error checking

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,12 +1,16 @@
 class StocksController < ApplicationController
 
   def search
+    @stock = Stock.new_from_lookup(params[:stock])
+     # render json: @stock
+
     if params[:stock].blank?
       flash.now[:danger] = "You have entered an empty string"
     else
       @stock = Stock.new_from_lookup(params[:stock])
       flash.now[:danger] = "You have entered an incorrect symbol" unless @stock
     end
+    # render partial: 'users/result'
     respond_to do |format|
       format.js { render partial: 'users/result' }
     end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -5,12 +5,13 @@ class Stock < ApplicationRecord
   # $Stock = StockQuote::Stock.new(api_key: pk_10967377fae34442a5612ba55b1159ab)
   def self.new_from_lookup(ticker_symbol)
     begin
+      StockQuote::Stock.new(api_key: 'pk_10967377fae34442a5612ba55b1159ab' )
       looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
-      price = strip_commas(looked_up_stock.l)
-      new(name: looked_up_stock.name, ticker: looked_up_stock.symbol, last_price: price)
-    rescue Exception => e
-      return nil
-    end
+      # price = strip_commas(looked_up_stock.latest_price)
+      new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol, last_price: looked_up_stock.latest_price)
+    # rescue Exception => e
+    #   return nil
+     end
   end
 
   def self.strip_commas(number)
