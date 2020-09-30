1349ae78dcd7e02da5ccf940b331bf1d11ee8091
Peyal <mohammad.piyal@gmail.com>
2020-09-30 12:28:46 +0600

    Add association, users and views

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,11 +1,13 @@
 class Stock < ApplicationRecord
   has_many :user_stocks
   has_many :users, through: :user_stocks
-  #
-  # $Stock = StockQuote::Stock.new(api_key: pk_10967377fae34442a5612ba55b1159ab)
+
+  def self.find_by_ticker(ticker_symbol)
+    where(ticker: ticker_symbol).first
+  end
   def self.new_from_lookup(ticker_symbol)
     begin
-      StockQuote::Stock.new(api_key: 'pk_10967377fae34442a5612ba55b1159ab' )
+      # StockQuote::Stock.new(api_key: 'pk_10967377fae34442a5612ba55b1159ab' )
       looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
       # price = strip_commas(looked_up_stock.latest_price)
       new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol, last_price: looked_up_stock.latest_price)
