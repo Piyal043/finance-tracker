a3cef69b518dd4506164494979e92be8478dd05a
Peyal <mohammad.piyal@gmail.com>
2020-09-30 19:09:42 +0600

    Search actions and errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,19 @@ class UsersController < ApplicationController
   end
 
   def my_friends
+    @friendships = current_user.friends
+  end
 
+  def search
+    if params[:search_param].blank?
+      flash.now[:danger] = "You have entered an empty string"
+    else
+      @users = User.search(params[:search_param])
+      flash.now[:danger] = "No users match this search criteria" if @users.blank?
+    end
+    # render partial: 'users/result'
+    respond_to do |format|
+      format.js { render partial: 'friends/result' }
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
   get 'my_portfolio', to: "users#my_portfolio"
   get 'search_stocks', to: 'stocks#search'
   get 'my_friends', to: "users#my_friends"
-  resources :user_stocks, only: [:create, :destroy]
+  resources :user_stocks, except: [:show, :edit, :update]
+  resources :users, only: [:show]
+  resources :friendships
+  get 'search-friends', to: 'users#search'
 end
