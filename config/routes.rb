9068496f1212831d5f70e3473cafd6ce61125c91
Peyal <mohammad.piyal@gmail.com>
2020-10-01 13:26:43 +0600

    Friend's functionality added

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   resources :users, only: [:show]
   resources :friendships
   get 'search-friends', to: 'users#search'
+  post 'add_friend', to: 'users#add_friend'
 end
